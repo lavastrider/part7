913d332ca4438e81001020c30f22861cbb2eea2a
lavastrider <70551407+lavastrider@users.noreply.github.com>
2023-03-22 18:11:40 -0500

    solved 7.7

--- country-hook/src/App.js
+++ country-hook/src/App.js
@@ -18,14 +18,34 @@ const useField = (type) => {
 const useCountry = (name) => {
   const [country, setCountry] = useState(null)
 
-  useEffect(() => {})
+  useEffect(() => {
+  if (name) {
+    axios
+      .get(`https://restcountries.com/v3.1/name/${name}?fullText=true`)
+      .then((response)=> {
+        console.log(response.data, 'is response data in useeffect in usecountry')
+        setCountry(response)
+      })
+      .catch(() => {
+        setCountry('not found')
+      })
+    }
+  }, [name])
 
   return country
 }
 
 const Country = ({ country }) => {
+  
+  console.log(country, 'is country in Country component')
+  
+  
   if (!country) {
     return null
+  } else if (country.data) {
+    console.log(country.data, 'is country data')
+    console.log('country data exists')
+    country.found = true
   }
 
   if (!country.found) {
@@ -38,10 +58,10 @@ const Country = ({ country }) => {
 
   return (
     <div>
-      <h3>{country.data.name} </h3>
-      <div>capital {country.data.capital} </div>
-      <div>population {country.data.population}</div> 
-      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+      <h3>{country.data[0].name.official} </h3>
+      <div>capital {country.data[0].capital} </div>
+      <div>population {country.data[0].population}</div> 
+      <img src={country.data[0].flags.png} height='100' alt={`flag of ${country.data[0].name.official}`}/>
     </div>
   )
 }
@@ -54,6 +74,9 @@ const App = () => {
   const fetch = (e) => {
     e.preventDefault()
     setName(nameInput.value)
+    console.log('we arrived at fetch')
+    console.log(country, 'is country from useCountry in app')
+    console.log(name, 'is name in fetch')
   }
 
   return (
